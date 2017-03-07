8106753889a90493d13d1ee584458f0dcd56713b
rwmodeltrain <rwmodeltrain@gmail.com>
2017-03-08 00:12:34 +0100

    Add files via upload

--- pipath/train.sh
+++ pipath/train.sh
@@ -3,5 +3,12 @@ sudo modprobe fbtft_device name=sainsmart18 rotate=270
 sleep 3
 con2fbmap 1 1
 sleep 2
-amixer set PCM -- 1000
+amixer set PCM -- 0
 sudo mount.cifs //192.168.1.61/Data /mnt/koploper -o username=username,password=password
+
+if [ -f master.txt ]
+then
+sudo python 1.py
+else
+sudo -u pi python 1.py
+fi
