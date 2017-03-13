35a33ba464151dc3d21a2a0bcb94e66ca175b1ff
rwmodeltrain <rwmodeltrain@gmail.com>
2017-03-13 23:06:10 +0100

    Update train.sh

--- pipath/train.sh
+++ pipath/train.sh
@@ -4,7 +4,7 @@ sleep 3
 con2fbmap 1 1
 sleep 2
 amixer set PCM -- 0
-sudo mount.cifs //192.168.1.61/Data /mnt/koploper -o username=username,password=password
+sudo mount.cifs //192.168.1.61/Koploper /mnt/koploper -o username=username,password=password
 
 if [ -f master.txt ]
 then
